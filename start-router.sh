ff5f5f5871b557f610b9022bf98b88818330633f
Daniel Crha <daniel.crha@gmail.com>
2019-10-22 01:33:13 +0200

    Init commit

---
+++ start-router.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# IP: 10.0.0.X/24
+# GW: 10.0.0.1
+# DNS: 10.0.0.1
+# X + 10 + last number of UID (2) so 10.0.0.12?
+# MAC addr: 52:54:00:36:12:01
+
+# set high VNC port
+
+vm_name="crhad-router"
+
+qemu-system-x86_64 \
+	-name "$vm_name" \
+	-vnc :83 \
+	-drive file=arch_img.raw,format=raw \
+	-m 1G \
+	-nic vde,sock=/tmp/vde-backbone.sock,mac=52:54:00:36:12:01 \
+	-nic vde,sock=/home/crhad/vde-crhad-1.sock \
+	-enable-kvm 
+	#-cdrom archlinux-2019.10.01-x86_64.iso
+
+
+
