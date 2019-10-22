bc90f22305bf67e7a01440f8fcf60a5ed44baa2f
Daniel Crha <daniel.crha@gmail.com>
2019-10-22 02:31:05 +0200

    Add communication between router and clone VM

--- start-clone.sh
+++ start-clone.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# IP: 10.0.0.X/24
-# GW: 10.0.0.1
-# DNS: 10.0.0.1
-# X + 10 + last number of UID (2) so 10.0.0.12?
-# MAC addr: 52:54:00:36:12:01
-
-# set high VNC port
+# IP: 10.0.X.2/24
+# GW: 10.0.0.12
+# DNS: 10.0.0.1 or [MY DNS]
+# X + 10 + last number of UID (2) so 10.0.0.12
 
 vm_name="crhad-clone"
 
@@ -15,7 +12,7 @@ qemu-system-x86_64 \
 	-vnc :84 \
 	-drive file=arch_clone_img.raw,format=raw \
 	-m 1G \
-	-nic vde,sock=/home/crhad/vde-crhad-1.sock \
+	-nic vde,sock=/home/crhad/vde-crhad-1.sock,mac=52:54:00:00:12:02 \
 	-enable-kvm 
 
 
--- start-router.sh
+++ start-router.sh
@@ -16,7 +16,7 @@ qemu-system-x86_64 \
 	-drive file=arch_img.raw,format=raw \
 	-m 1G \
 	-nic vde,sock=/tmp/vde-backbone.sock,mac=52:54:00:36:12:01 \
-	-nic vde,sock=/home/crhad/vde-crhad-1.sock \
+	-nic vde,sock=/home/crhad/vde-crhad-1.sock,mac=52:54:00:00:12:01 \
 	-enable-kvm 
 	#-cdrom archlinux-2019.10.01-x86_64.iso
 
